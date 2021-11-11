96cdc54b0bdfe084da2ee09b7764cd5153c82c4d
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 14:04:45 -0800

    Initial dotnet

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ cd working
 
 # Checkout upstream
 
-# git clone --depth 1 --branch main https://github.com/upstream/upstream.git
-# cd upstream
+git clone --depth 1 --branch main https://github.com/dotnet/dotnet-docker.git
+cd dotnet-docker
 
 # Transform
 
-# sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" 17/bullseye/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/" runtime-deps/3.1/bullseye-slim/amd64/Dockerfile
 
 # Build
 
-# docker build --tag ghcr.io/golden-containers/upstream-bullseye-slim:17 17/bullseye-slim
+docker build --tag ghcr.io/golden-containers/dotnet/runtime-deps:3.1-bullseye-slim runtime-deps/3.1/bullseye-slim/amd64/Dockerfile
 
 # Push
 
-# docker push ghcr.io/golden-containers/upstream-bullseye-slim:17
+docker push ghcr.io/golden-containers/dotnet -a
