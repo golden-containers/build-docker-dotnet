ac829b02cc5e668a5940feea4b0f963d0621a0f0
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 13:33:43 -0800

    Clean up

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ rm -rf working
 mkdir working
 cd working
 
+GCI_URL="ghcr.io/golden-containers"
+
 # Checkout upstream
 
 git clone --depth 1 --branch main https://github.com/dotnet/dotnet-docker.git
@@ -12,27 +14,29 @@ cd dotnet-docker
 
 # Transform
 
+GCI_REGEX_URL=$(echo ${GCI_URL} | sed 's/\//\\\//g')
+
 # This sed syntax is GNU sed specific
 [ -z "$(command -v gsed)" ] && GNU_SED=sed || GNU_SED=gsed
 
 ${GNU_SED} -i \
-    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/; t" \
-    -e "1,// s//FROM ghcr.io\/golden-containers\/debian\:bullseye-slim/" \
+    -e "1 s/FROM.*/FROM ${GCI_REGEX_URL}\/debian\:bullseye-slim/; t" \
+    -e "1,// s//FROM ${GCI_REGEX_URL}\/debian\:bullseye-slim/" \
     src/runtime-deps/3.1/bullseye-slim/amd64/Dockerfile
 
 ${GNU_SED} -i \
-    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/dotnet\/runtime-deps\:3.1-bullseye-slim/; t" \
-    -e "1,// s//FROM ghcr.io\/golden-containers\/dotnet\/runtime-deps\:3.1-bullseye-slim/" \
+    -e "1 s/FROM.*/FROM ${GCI_REGEX_URL}\/dotnet\/runtime-deps\:3.1-bullseye-slim/; t" \
+    -e "1,// s//FROM ${GCI_REGEX_URL}\/dotnet\/runtime-deps\:3.1-bullseye-slim/" \
     src/runtime/3.1/bullseye-slim/amd64/Dockerfile
 
 ${GNU_SED} -i \
-    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/dotnet\/runtime\:3.1-bullseye-slim/; t" \
-    -e "1,// s//FROM ghcr.io\/golden-containers\/dotnet\/runtime\:3.1-bullseye-slim/" \
+    -e "1 s/FROM.*/FROM ${GCI_REGEX_URL}\/dotnet\/runtime\:3.1-bullseye-slim/; t" \
+    -e "1,// s//FROM ${GCI_REGEX_URL}\/dotnet\/runtime\:3.1-bullseye-slim/" \
     src/aspnet/3.1/bullseye-slim/amd64/Dockerfile
 
 ${GNU_SED} -i \
-    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" \
-    -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" \
+    -e "1 s/FROM.*/FROM ${GCI_REGEX_URL}\/buildpack-deps\:bullseye-scm/; t" \
+    -e "1,// s//FROM ${GCI_REGEX_URL}\/buildpack-deps\:bullseye-scm/" \
     src/sdk/3.1/bullseye/amd64/Dockerfile
 
 # Build
@@ -40,13 +44,19 @@ ${GNU_SED} -i \
 [ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
 
 BUILD_PLATFORM=" --platform linux/amd64 "
-GCI_URL="ghcr.io/golden-containers"
 BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
 
-docker build src/runtime-deps/3.1/bullseye-slim/amd64/ --tag ${GCI_URL}/dotnet/runtime-deps:3.1-bullseye-slim ${BUILD_ARGS}
-docker build src/runtime/3.1/bullseye-slim/amd64/ --tag ${GCI_URL}/dotnet/runtime:3.1-bullseye-slim ${BUILD_ARGS}
-docker build src/aspnet/3.1/bullseye-slim/amd64/ --tag ${GCI_URL}/dotnet/aspnet:3.1-bullseye-slim ${BUILD_ARGS}
-docker build src/sdk/3.1/bullseye/amd64/ --tag ${GCI_URL}/dotnet/sdk:3.1-bullseye ${BUILD_ARGS}
+docker build src/runtime-deps/3.1/bullseye-slim/amd64/ ${BUILD_ARGS} \
+    --tag ${GCI_URL}/dotnet/runtime-deps:3.1-bullseye-slim
+    
+docker build src/runtime/3.1/bullseye-slim/amd64/ ${BUILD_ARGS} \
+    --tag ${GCI_URL}/dotnet/runtime:3.1-bullseye-slim
+
+docker build src/aspnet/3.1/bullseye-slim/amd64/ ${BUILD_ARGS} \
+    --tag ${GCI_URL}/dotnet/aspnet:3.1-bullseye-slim
+
+docker build src/sdk/3.1/bullseye/amd64/ ${BUILD_ARGS} \
+    --tag ${GCI_URL}/dotnet/sdk:3.1-bullseye
 
 # Push
 
